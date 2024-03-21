f750e108f60250f1c8866ca69f0235154d295431
dimitriospanopoulos <dimitrios.panopoulos@eeas.europa.eu>
2024-03-21 09:46:16 +0100

    add binance ticker every minute to fetch pairs

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from db.database import setup_database
+from utils import ticker_client
 from utils.websocket_client import start_websocket
 
 def main():
     setup_database()
-    start_websocket()
+    #start_websocket()
+    ticker_client.start_ticker()
 
 if __name__ == "__main__":
     main()
