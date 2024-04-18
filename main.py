735b70f2c6dafa375da175dfd5f67b6faa52bd74
Dimitrios Panopoulos <d4pano@gmail.com>
2024-04-18 07:52:34 +0200

    uncomment ticker

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utils.websocket_client import start_websocket
 def main():
     setup_database()
     #start_websocket()
-    #ticker_client.start_ticker()
+    ticker_client.start_ticker()
 
 if __name__ == "__main__":
     main()
