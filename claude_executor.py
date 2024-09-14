2966ca148f75f98b78e545280466b13eaf0162b3
dimitriospanopoulos <dimitrios.panopoulos@eeas.europa.eu>
2024-09-14 10:25:20 +0200

    Telegrame claude start

--- claude_executor.py
+++ claude_executor.py
@@ -1,10 +1,21 @@
 
-
+import asyncio
+from configuration.telegram_config import config as telegram_config
+from notifications.telegram import send_telegram_message
 from trading_bot.claude import CryptoTradingBot
+import logging
+logging.basicConfig(filename='trading_bot.log', level=logging.INFO, 
+                    format='%(asctime)s %(message)s')
 
+def telegram(message):
+    config = telegram_config()
+    asyncio.run(send_telegram_message(config['token'], config['chat_id'], message))
+    logging.info(f"Telegram message sent: {message}")
 
 if __name__ == "__main__":
-    
+    heartbeat = 0
+    telegram('Claude BOT started')
+    logging.info('Claude BOT started')
         # Define trading pairs with their individual take-profit and diversification percentages
     trading_pairs_config = {
         'BTCUSDT': {
