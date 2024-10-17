1848fa4366754c521031886257d1f518a4809765
LAPTOP-9PC8DMT2\panopdi <dimitrios.panopoulos@eeas.europa.eu>
2024-10-17 09:12:37 +0200

    add angular

--- ui/bot-dashboard/src/app/app.routes.ts
+++ ui/bot-dashboard/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { DetailsComponent } from './features/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        component: DashboardComponent,
+        children: [
+            {
+                path: 'child',
+                loadComponent: () => 
+                    import('./features/dashboard/child/child.component')
+                        .then(m => m.ChildComponent)
+            }
+        ]
+    },
+    {
+        path: 'details',
+        component: DetailsComponent
+    },
+];
