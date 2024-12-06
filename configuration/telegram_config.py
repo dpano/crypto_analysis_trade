08d7c4951745ecfcb3baf01103366deb7a56fc52
LAPTOP-9PC8DMT2\panopdi <dimitrios.panopoulos@eeas.europa.eu>
2024-12-06 16:34:31 +0100

    clean up

--- configuration/telegram_config.py
+++ configuration/telegram_config.py
@@ -5,5 +5,3 @@ def config():
         'token': os.environ.get('TELEGRAM_TOKEN'),
         'chat_id': os.environ.get('TELEGRAM_CHAT_ID')
     }
-    #'7054122444:AAHitQ3o2zl_TIhqIL86ha4QZWCtLrMUxbY'
-    #'7078347828'
